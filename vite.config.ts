223140f387e95cd83c16734d6ab81e93fcfc2fcb
YanHSama <1269987538@qq.com>
2026-03-19 06:58:00 +0800

    feat: vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'vue-router/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    // 必须要在 vue 插件之前
+    VueRouter({
+      routesFolder: 'src/pages', // 默认 pages，可改
+      extensions: ['.vue'], // 匹配文件后缀
+      dts: 'src/typed-router.d.ts', // 生成类型文件
+       // 添加调试选项
+      logs: true
+    }),
     vue(),
     vueJsx(),
     vueDevTools(),
