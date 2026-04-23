e5eefa8dca9a735afb96f11c56ffbbc818429b1a
YanHSama <1269987538@qq.com>
2026-04-23 17:19:44 +0800

    feat: defineModel 关于ts注解

--- src/pages/cloud/components/tabTwo.vue
+++ src/pages/cloud/components/tabTwo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="text" name="age" v-model="age" />
+    <input type="text" name="age" v-model.number="age" />
     <input type="text" name="dec" v-model="dec" />
   </div>
 </template>
@@ -8,7 +8,27 @@
 const age = defineModel<number>("age", {
   default: 20,
 });
-const dec = defineModel<string>("dec", {
+const [dec, modifierDec] = defineModel<string, string>("dec", {
   required: true,
+  get(value: string): string {
+    console.log(value, "get-xxx", modifierDec);
+    return modifierDec?.upper ? value.toUpperCase() : value;
+  },
+  set(value: string) {
+    console.log(value, "xxx");
+
+    return modifierDec?.upper ? value.toUpperCase() : value;
+  },
+});
+
+console.log(dec, "xxx", modifierDec);
+
+const reset = () => {
+  age.value = 20;
+  dec.value = "DESC";
+};
+
+defineExpose({
+  reset,
 });
 </script>
--- src/pages/cloud/index.vue
+++ src/pages/cloud/index.vue
@@ -1,14 +1,18 @@
 <template>
   <p>{{ "这里是云平台首页" }}</p>
-  <tabTwo v-model:age="age" v-model:dec="dec"> </tabTwo>
+  <tabTwo ref="tabTwoRef" v-model:age="age" v-model:dec.upper="dec"> </tabTwo>
+  <button @click="reset">重置</button>
 </template>
 <script setup lang="ts">
 import tabTwo from "@/pages/cloud/components/tabTwo.vue";
 import { ref } from "vue";
-
+const tabTwoRef = ref<typeof tabTwo>();
 const age = ref();
 const dec = ref("desc");
 
+const reset = () => {
+  tabTwoRef.value?.reset();
+};
 defineOptions({
   name: "CloudIndexView",
 });
