0293d428b24e0c65b64c9fe1f3dcbdc93c69414c
YanHSama <1269987538@qq.com>
2026-05-19 19:52:46 +0800

    chore: build

---
+++ dist-cube/public/role-lDMl90H8.js
@@ -0,0 +1,17 @@
+/* See licenses of bundled dependencies at https://example.com/license.md */
+import { L as openBlock, b as defineComponent, m as createElementBlock } from "./runtime-core.esm-bundler-DSU8tT8U.js";
+//#endregion
+//#region src/pages/cloud/role/index.vue
+var role_default = /* @__PURE__ */ defineComponent({
+	name: "CloudRoleIndexView",
+	__name: "index",
+	setup(__props) {
+		return (_ctx, _cache) => {
+			return openBlock(), createElementBlock("div");
+		};
+	}
+});
+//#endregion
+export { role_default as default };
+
+//# sourceMappingURL=role-lDMl90H8.js.map
